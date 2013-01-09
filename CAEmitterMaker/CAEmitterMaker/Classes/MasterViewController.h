0c2a333cdcf88a4fffd303f2fa83f0d85c5a5bb4
Nick Brice <nick.brice88@gmail.com>
2013-01-08 22:34:03 -0800

    Added emtpy root view controller

---
+++ CAEmitterMaker/CAEmitterMaker/Classes/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  CAEmitterMaker
+//
+//  Created by Nick Brice on 1/8/13.
+//  Copyright (c) 2013 Nick Brice. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MasterViewController : NSViewController
+
+@end
---
+++ CAEmitterMaker/CAEmitterMaker/Classes/MasterViewController.m
@@ -0,0 +1,27 @@
+//
+//  MasterViewController.m
+//  CAEmitterMaker
+//
+//  Created by Nick Brice on 1/8/13.
+//  Copyright (c) 2013 Nick Brice. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@end
+
+@implementation MasterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
